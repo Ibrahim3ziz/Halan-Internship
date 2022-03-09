d48bec39f2d5a357c6fd7468d6ef63ee25c3534b
Ibrahim Abdul Aziz <ichbinhima@gmail.com>
2022-03-09 16:15:24 +0200

    DIP Done

--- Week One/SOLID/DIP.playground/Contents.swift
+++ Week One/SOLID/DIP.playground/Contents.swift
@@ -2,4 +2,55 @@
 
 import UIKit
 
-// States that 
+// States that high level modules shouldn't depend on lower level modules, Both sould depend on abstracion layer
+// Abstraction layer shouldn't depend on details, Details should depend on abstraction.
+// DECOUPLING DEPENDENCIES
+
+
+// Higher Level Module which can not be used in other projects as it is tightly coupled with FileManger()
+
+class Handler {
+    let fm = FileManger()
+    
+    func handle(string: String) {
+        fm.save(string: string)
+    }
+}
+
+
+// Lower Level Module which we can use in other project.
+class FileManger {
+    func save(string: String) {
+        // Open a file
+        // Save the string in this file
+        // Close the file
+    }
+}
+
+
+
+// Abstraction layer
+protocol Storage {
+    func save(string: String)
+}
+
+
+class HandlerDIP {
+    
+    func save(string: String) {
+        
+    }
+    
+    let storage: Storage
+    
+    init(storage: Storage) {
+        self.storage = storage
+    }
+    
+    func handle(string: String) {
+        storage.save(string: string)
+    }
+    
+}
+
+
